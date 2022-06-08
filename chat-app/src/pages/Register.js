f18de0da0c4377ac87ff25871257b48fd7683a0d
pepermz <jose.ram.18@gmail.com>
2022-06-08 16:21:19 -0600

    added styling to the register form

--- chat-app/src/pages/Register.js
+++ chat-app/src/pages/Register.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from "styled-components"
 import { Link } from 'react-router-dom';
+import Logo from "../assets/project4.png"
 
 const handleSubmit =(event)=>{
     event.preventDefault();
@@ -17,7 +18,7 @@ function Register() {
         <FormContainer>
             <form onSubmit={(event)=>handleSubmit(event)}>
                 <div className="brand">
-                    <img src="" alt="" />
+                    <img src={Logo} alt="LOGO" />
                     <h1>ChatApp</h1>
                 </div>
                 <input type="text" placeholder='Username' name="username" onChange={e=>handleChange(e)} />
@@ -32,6 +33,63 @@ function Register() {
   )
 }
 
-const FormContainer = styled.div``;
+const FormContainer = styled.div`
+    height: 100vh;
+    width: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 1rem;
+    align-items: center;
+    background-color: #131324;
+    .brand {
+        display:flex;
+        align-items: center;
+        gap: 1rem;
+        justify-content: center;
+        img{
+            height: 5rem;
+        }
+        h1 {
+            color: white;
+            text-transform: uppercase;;
+        }
+    }
+    form{
+        display:flex;
+        flex-direction: column;
+        gap: 2rem;
+        background-color: #00000076;
+        border-radius: 2rem;
+        padding: 3rem 5rem;
+        input {
+            background-color: transparent;
+            padding: 1rem;
+            border: 0.1rem solid #4e0eff;
+            border-radius: 0.4;
+            color: white;
+            width: 100%;
+            font-size: 1rem;
+            &:focus {
+                border: 0.1rem solid #997af0;
+                outline: none;
+            }
+        }
+        button {
+            background-color: #997af0;
+            color: white;
+            padding: 1rem 2rem;
+            border: none;
+            font-weight: bold;
+            cursor: pointer;
+            border-radius: 0.4rem;
+            font-size: 1rem;
+            text-transform: uppercase;
+            &:hover {
+                background-color: #4e0eff;
+            }
+        }
+    }
+`;
 
 export default Register

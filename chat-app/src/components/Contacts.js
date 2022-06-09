0f3217513f7a6c2d34d970a1c7793f78be201885
pepermz <jose.ram.18@gmail.com>
2022-06-09 15:38:11 -0600

    added scrollbar

--- chat-app/src/components/Contacts.js
+++ chat-app/src/components/Contacts.js
@@ -51,7 +51,7 @@ export default function Contacts({contacts, currentUser}) {
                   <img src={`data:image/svg+xml;base64,${currentUserImage}`} alt="avatar"/>    
               </div>
               <div className='username'>
-                  <h3>{currentUserName}</h3>
+                  <h2>{currentUserName}</h2>
               </div>
           </div>
         </Container>
@@ -86,14 +86,62 @@ const Container = styled.div`
       align-items: center;
       overflow: auto;
       gap: 0.8rem;
+      &::-webkit-scrollbar {
+        width: 0.2rem;
+        &-thumb {
+          background-color: #ffffff39;
+          width: 0.1rem;
+          border-radius: 1rem;
+        }
+      }
       .contact{
         background-color: #ffffff39;
         min-height: 5rem;
         width: 90%;
+        cursor: pointer;
+        border-radius: 0.2rem;
+        padding: 0.4rem;
+        gap: 1rem;
+        align-items: center;
+        display:flex;
+        transition: 0.5s ease-in-out;
         .avatar{
           img{
             height:3rem;
           }
+        }
+        .username{
+          h3{
+            color:white;
+          }
+        }
+      }
+    .selected{
+      background-color: #9186f3;
+    }
+  }
+  .current-user {
+    background-color: #0d0d30;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 2rem;
+    .avatar {
+      img {
+        height: 4rem;
+        max-inline-size: 100%;
+      }
+    }
+    .username {
+      h2 {
+        color: white;
+      }
+    }
+    @media screen and (min-width:720px) and (max-width:1080px);
+      gap: 0.5rem;
+      .username{
+        h2{
+          font-size: 1rem;
         }
       }
   }

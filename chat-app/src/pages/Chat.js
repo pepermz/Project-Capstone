eb6a635b32c4e0ba799df5fe04d9e91328e38e8d
pepermz <jose.ram.18@gmail.com>
2022-06-09 19:07:49 -0600

    fixed username error

--- chat-app/src/pages/Chat.js
+++ chat-app/src/pages/Chat.js
@@ -5,12 +5,14 @@ import {useNavigate} from 'react-router-dom'
 import { allUsersRoute } from '../utils/APIRoutes';
 import Contacts from '../components/Contacts';
 import Welcome from '../components/Welcome';
+import ChatContainer from '../components/ChatContainer';
 
 function Chat() {
   const navigate = useNavigate()
   const [contacts, setContacts] = useState([])
   const [currentUser, setCurrentUser] = useState(undefined)
   const [currentChat, setCurrentChat] = useState(undefined)
+  const [isLoaded, setIsLoaded] = useState(false)
 
   useEffect( ()=> {
         async function fetchData(){
@@ -18,6 +20,7 @@ function Chat() {
             navigate('/login')
         } else {
           setCurrentUser(await JSON.parse(localStorage.getItem("chat-app-user")))
+          setIsLoaded(true)
         }
         }
         fetchData()
@@ -46,7 +49,13 @@ function Chat() {
     <Container>
       <div className='container'>
         <Contacts contacts={contacts} currentUser={currentUser} changeChat={handleChatChange}/>
-        <Welcome currentUser={currentUser}/>
+        {
+          isLoaded && currentChat === undefined ? (
+            <Welcome currentUser={currentUser} />
+          ) : (
+            <ChatContainer currentUser={currentUser} />
+          )
+        }
       </div>
     </Container>
   )

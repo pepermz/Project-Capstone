797d29a9743479d241881a0d5ea20606a3614f53
pepermz <jose.ram.18@gmail.com>
2022-06-10 11:09:16 -0600

    got to send messages

--- chat-app/src/components/ChatContainer.js
+++ chat-app/src/components/ChatContainer.js
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import styled from 'styled-components'
 import ChatInput from './ChatInput';
 import Logout from './Logout';
-import Messages from './Messages';
+// import Messages from './Messages';
 import axios from 'axios';
-import { sendMessageRoute } from '../utils/APIRoutes';
+import { getAllMessagesRoute, sendMessageRoute } from '../utils/APIRoutes';
+
 
 
 export default function ChatContainer({currentChat, currentUser}) {
+
+    const [messages, setMessages] =useState([])
+
+
+    useEffect(()=>{
+        async function fetchData(){
+        //fetch the chat of the current user and the current chat user
+        const response = await axios.post(getAllMessagesRoute, {
+            from: currentUser._id,
+            to: currentChat._id,
+        })
+        setMessages(response.data)
+    }
+        fetchData()
+    },[currentChat])
+
     //Header  , Chat container , Chat input will be handled here
     const handleSendMsg = async(msg) => {
         //Magic goes here
@@ -33,8 +50,24 @@ export default function ChatContainer({currentChat, currentUser}) {
                             </div>
                             <Logout />
                         </div>
-                        <Messages />
-                        <div className='chat-messages'></div>
+                        <div className='chat-messages'>
+                            {
+                                messages.map((message) =>{
+                                    return (
+                                        <div>
+                                            <div className={`message ${message.fromSelf ? "sended": "received"}`}>
+                                                <div className='content'>
+                                                    <p>
+                                                        {message.message}
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                        {/* <Messages /> */}
                         <ChatInput handleSendMsg ={handleSendMsg}/>
                     </Container>
                 )
@@ -44,28 +77,73 @@ export default function ChatContainer({currentChat, currentUser}) {
 }
 
 const Container = styled.div`
-    padding: 1rem;
-    .chat-header{
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        padding: 0 2rem;
-        .user-details {
-            display: flex;
-            align-items: center;
-            gap: 1rem;
-            .avatar {
-                img {
-                    height: 3rem;
-                }
-            }
-            .username {
-                h3 {
-                    color: white;
-                }
-            }
+    padding-top: 1rem;
+    display: grid;
+    grid-template-rows: 10% 78% 12%;
+    gap: 0.1rem;
+    overflow: hidden;
+  .chat-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 2rem;
+    .user-details {
+      display: flex;
+      align-items: center;
+      gap: 1rem;
+      .avatar {
+        img {
+          height: 3rem;
         }
+      }
+      .username {
+        h3 {
+          color: white;
+        }
+      }
     }
-
-
+  }
+  .chat-messages {
+    padding: 1rem 2rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    overflow: auto;
+    &::-webkit-scrollbar {
+      width: 0.2rem;
+      &-thumb {
+        background-color: #ffffff39;
+        width: 0.1rem;
+        border-radius: 1rem;
+      }
+    }
+    .message {
+      display: flex;
+      align-items: center;
+      .content {
+        max-width: 40%;
+        overflow-wrap: break-word;
+        padding: 1rem;
+        font-size: 1.1rem;
+        border-radius: 1rem;
+        color: #d1d1d1;
+        @media screen and (min-width: 720px) and (max-width: 1080px) {
+          max-width: 70%;
+        }
+      }
+    }
+    .sended {
+      justify-content: flex-end;
+      .content {
+        background-color: #4f04ff21;
+      }
+    }
+    .recieved {
+      justify-content: flex-start;
+      .content {
+        background-color: #9900ff20;
+      }
+    }
+  }
 `;
+ 
